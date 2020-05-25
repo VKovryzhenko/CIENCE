93cacb10e50a387f2f74b0b4ae2052755bdf5219
Viktor <viktor.kovryzhenko@eteam.io>
2020-05-25 14:10:58 +0300

    first commit

---
+++ Reset_password.js
@@ -0,0 +1,9 @@
+describe('Page of manager',() => {
+ 
+    it('Select Team', () => {
+     cy.visit('http://relax-qa.cienceapps.com')
+     cy.get('.sc-fzowVh').type('viktor.kovryzhenko@eteam.io')
+     cy.get('.MuiButton-label').click()
+    })
+  })
+ 
